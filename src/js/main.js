296871faeb11cae524c695adc3df40d0ed87a37a
Таня Наумчук <tatiana.naumchuk123456789@gmail.com>
2022-04-01 21:01:10 +0300

    structure_desk

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,24 @@
 const toggleMenu = document.querySelector('.toggle');
 const menu = document.querySelector('.menu__list');
 
-function toggleFunction(elem) {
+function toggleFunction() {
 	if (this) menu.classList.toggle('menu__list-is-active');
 }
 
 toggleMenu.addEventListener('click', toggleFunction);
+
+const selectedSructure = document.querySelectorAll('.active');
+const targetItem = document.querySelectorAll('.item');
+
+targetItem.forEach(elem => {
+	elem.addEventListener('click', selectContent);
+});
+
+function selectContent() {
+	//console.log(this.dataset.title);
+	selectedSructure.forEach(elem => {
+		if (this.dataset.title === elem.dataset.content) {
+			elem.classList.toggle('active-selected');
+		}
+	});
+}
