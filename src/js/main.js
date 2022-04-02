691770a317564c40cd90cdbc9efa32f31a19393f
Таня Наумчук <tatiana.naumchuk123456789@gmail.com>
2022-04-02 13:47:23 +0300

    section_structure

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,16 @@
 const toggleMenu = document.querySelector('.toggle');
 const menu = document.querySelector('.menu__list');
-
+toggleMenu.addEventListener('click', toggleFunction);
 function toggleFunction() {
 	if (this) menu.classList.toggle('menu__list-is-active');
 }
 
-toggleMenu.addEventListener('click', toggleFunction);
-
 const selectedSructure = document.querySelectorAll('.active');
 const targetItem = document.querySelectorAll('.item');
-
 targetItem.forEach(elem => {
 	elem.addEventListener('click', selectContent);
 });
-
 function selectContent() {
-	//console.log(this.dataset.title);
 	selectedSructure.forEach(elem => {
 		if (this.dataset.title === elem.dataset.content) {
 			elem.classList.toggle('active-selected');
