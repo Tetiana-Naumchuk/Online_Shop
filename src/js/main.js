afb14d8d8b7fc9b8e738efac946a67af8e8a9e38
Таня Наумчук <tatiana.naumchuk123456789@gmail.com>
2022-04-04 03:16:28 +0300

    section-stages

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,22 @@
-// const toggleMenu = document.querySelector('.toggle');
-// const menu = document.querySelector('.menu__list');
-// toggleMenu.addEventListener('click', toggleFunction);
-// function toggleFunction() {
-// 	if (this) menu.classList.toggle('menu__list-is-active');
-// }
+const toggleMenu = document.querySelector('.toggle');
+const menu = document.querySelector('.menu__list');
+toggleMenu.addEventListener('click', toggleFunction);
+function toggleFunction() {
+	if (this) menu.classList.toggle('menu__list-is-active');
+}
 
-// const selectedSructure = document.querySelectorAll('.active');
-// const targetItem = document.querySelectorAll('.item');
-// targetItem.forEach(elem => {
-// 	elem.addEventListener('click', selectContent);
-// });
-// function selectContent() {
-// 	selectedSructure.forEach(elem => {
-// 		if (this.dataset.title === elem.dataset.content) {
-// 			elem.classList.toggle('active-selected');
-// 		}
-// 	});
-// }
+const selectedSructure = document.querySelectorAll('.active');
+const targetItem = document.querySelectorAll('.item');
+targetItem.forEach(elem => {
+	elem.addEventListener('click', selectContent);
+});
+function selectContent() {
+	selectedSructure.forEach(elem => {
+		if (this.dataset.title === elem.dataset.content) {
+			elem.classList.toggle('active-selected');
+		}
+	});
+}
 
 const sliderBlock = document.querySelectorAll('.slider-block');
 let count = 0;
